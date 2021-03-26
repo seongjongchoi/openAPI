cd24f62798f9db43d94e46ac1772817901eed697
chois <chois@uos.ac.kr>
2021-03-26 13:31:32 +0900

    first version

---
+++ sitRepback.js
@@ -0,0 +1,21 @@
+/* NodeJs 샘플 코드 */
+
+
+var request = require('request');
+
+var url = 'http://apis.data.go.kr/1741000/MisfortuneSituationNoticeMsg2/getMisfortuneSituationNoticeMsgList';
+let sKey = "aN2aOKkivRPAL7ZlAtz6BKj%2FQcSYUvEvZVRk80qX0cTYMUEUAiGIx%2BslDNoLo1feNuxD6cSBgSaMO3B6tp%2FZvw%3D%3D";
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + sKey; /* Service Key*/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('xml'); /* */
+queryParams += '&' + encodeURIComponent('flag') + '=' + encodeURIComponent('Y'); /* */
+
+request({
+    url: url + queryParams,
+    method: 'GET'
+}, function (error, response, body) {
+    console.log('Status', response.statusCode);
+    console.log('Headers', JSON.stringify(response.headers));
+    console.log('Reponse received', body);
+});
---
+++ test.js
@@ -0,0 +1,10 @@
+array=[1, 3, 4];
+
+for (let ind = 0; ind < array.length; ind++) {
+    const element = array[ind];
+    console.log(element);
+    
+}
+
+console.log(0 || 0.5);
+console.log(0 || 'this is a test');
